18a582adc3d89b46299e2a0122311b09402374fe
Kevin Tindall <kevinkjt2000@gmail.com>
2020-10-23 21:03:46 -0500

    Use mysql connector to connect to database

--- redhat_star_wars/_database.py
+++ redhat_star_wars/_database.py
@@ -1,5 +1,13 @@
+import mysql.connector
 from ._settings import MYSQL_ROOT_PASSWORD
+
 
 class Database:
     def __init__(self):
-        raise NotImplementedError("Install mysql package first")
+        self._conn = mysql.connector.connect(host="127.0.0.1", user="root", password=MYSQL_ROOT_PASSWORD)
+        cursor = self._conn.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS starwars DEFAULT CHARACTER SET 'utf8'")
+
+    def __del__(self):
+        self._conn.close()
+        del self._conn
--- tests/integration/test_db.py
+++ tests/integration/test_db.py
@@ -1,6 +1,11 @@
+import pytest
 from redhat_star_wars._database import Database
 
 
 def test_connection_works():
     db = Database()
 
+
+def test_subsequent_connection_works_since_first_connection_populates_schemas():
+    db = Database()
+    db = Database()
