5ba05f073b3ed5513d59df04fad8a613d9f16538
Kevin Tindall <kevinkjt2000@gmail.com>
2020-10-25 22:55:52 -0500

    Assert RuntimeError is raised when connection attempts fail

--- tests/integration/test_db.py
+++ tests/integration/test_db.py
@@ -26,6 +26,23 @@ def test_waits_on_mysql_to_be_fully_operational(mock_connect, mock_sleep):
     Database()
 
 
+@patch("redhat_star_wars._database.sleep")
+@patch(
+    "redhat_star_wars._database.mysql.connector.connect",
+    side_effect=mysql.connector.errors.OperationalError(
+        errno=2013,
+        msg="2013 (HY000): Lost connection to MySQL server at 'reading initial communication packet', system error: 0",
+        sqlstate="HY000",
+    ),
+)
+def test_raises_runtime_exception_if_database_fails_to_connect_after_several_attempts(
+    mock_connect, mock_sleep
+):
+    with pytest.raises(RuntimeError):
+        Database()
+    assert mock_connect.call_count == 10
+
+
 def test_subsequent_connection_works_since_first_connection_populates_schemas():
     Database()
     Database()
